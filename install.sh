ff2cdce5f8245900f6a094be82614d124f5ff6d5
ryan <pohsienliu2012@gmail.com>
2016-06-26 22:51:46 +0800

    added install.sh and README

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo pip install slackbot 
+sudo pip install bs4
+sudo pip install unidecode
+sudo pip install selenium 
+git clone https://github.com/abenassi/Google-Search-API.git
+cd Google-Search-API
+sudo python setup.py install
--- slackbot/install.sh
+++
@@ -1,5 +0,0 @@
-sudo pip install slackbot 
-sudo pip install bs4
-sudo pip install unidecode
-sudo pip install selenium 
-sudo pip install Google-Search-API --upgrade
